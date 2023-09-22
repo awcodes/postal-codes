25a5c0ac9b7acd953d600dcc86f228c172e20e14
awcodes <awcodes@users.noreply.github.com>
2023-09-22 17:40:35 +0000

    Fix styling

--- database/factories/PostalCodeFactory.php
+++ database/factories/PostalCodeFactory.php
@@ -13,17 +13,17 @@ class PostalCodeFactory extends Factory
     {
         return [
             'country_code' => 'US',
-            'postal_code' => rand(10000,99999),
+            'postal_code' => rand(10000, 99999),
             'place_name' => $this->faker->city(),
             'state_name' => $this->faker->word(),
             'state' => str($this->faker->randomLetter().$this->faker->randomLetter())->upper(),
             'county_name' => $this->faker->word(),
-            'county_code' => rand(100,999),
+            'county_code' => rand(100, 999),
             'community_name' => null,
             'community_code' => null,
             'lat' => $this->faker->latitude(),
             'lng' => $this->faker->longitude(),
-            'accuracy' => rand(0,5),
+            'accuracy' => rand(0, 5),
         ];
     }
 }
--- tests/Unit/ModelTest.php
+++ tests/Unit/ModelTest.php
@@ -2,7 +2,7 @@
 
 use Awcodes\PostalCodes\Models\PostalCode;
 
-it('can generate model', function() {
+it('can generate model', function () {
     $code = PostalCode::factory()->create([
         'postal_code' => '31405',
         'state' => 'GA',
