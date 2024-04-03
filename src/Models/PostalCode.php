7f79c3fb9575e32f52deaeb37994417120381275
Adam Weston <awcodes1@gmail.com>
2024-04-03 11:33:58 -0400

    Fix: model factory

--- src/Models/PostalCode.php
+++ src/Models/PostalCode.php
@@ -2,12 +2,18 @@
 
 namespace Awcodes\PostalCodes\Models;
 
+use Awcodes\PostalCodes\Database\Factories\PostalCodeFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PostalCode extends Model
 {
     use HasFactory;
+
+    protected static function newFactory(): PostalCodeFactory
+    {
+        return new PostalCodeFactory();
+    }
 
     protected $fillable = [
         'country_code',
